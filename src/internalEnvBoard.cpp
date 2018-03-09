b506c6fd1b436e791322ec4d38faaf5dd4d093b9
David Herczeg <david.herczeg@live.ca>
2018-03-08 21:08:00 -0700

    another minor update on timer

--- src/internalEnvBoard.cpp
+++ src/internalEnvBoard.cpp
@@ -18,6 +18,7 @@ static constexpr float framerate = 100;
 
 // humidity/temperature sensor
 #define HIH7120ADDRESS 0x27
+
 // pressure/temperature sensor
 #define MPLADDRESS 0x60
 unsigned char buf[5];
@@ -36,12 +37,11 @@ pressure_StructDef pressure;
 
 uint8_t OUT_P_MSB, OUT_P_CSB, OUT_P_LSB;
 
-
 // calculates humidity from raw data
 float humidity(){
-   float hum = (buf[0] << 8) + buf[1];
-   float humidity = float(hum)/16382*100;
-   return humidity;
+    float hum = (buf[0] << 8) + buf[1];
+    float humidity = float(hum)/16382*100;
+    return humidity;
 }
 // calculates temperature from raw data
 float temp(){
@@ -49,66 +49,69 @@ float temp(){
     float temperature = (temp/16382*165)-40;
     return temperature;
 }
+
 //must be used before to tell the HIH7120 to make a measurement
 //there must be a delay between measurement request and retrieving
 //the measurement from the sensor which is built into the function
 //below
 void hum_measurement_req(){
-  Wire.beginTransmission(HIH7120ADDRESS);
-  Wire.write(HIH7120ADDRESS<<1);
-  Wire.endTransmission();
-  delay(50);
+    Wire.beginTransmission(HIH7120ADDRESS);
+    Wire.write(HIH7120ADDRESS<<1);
+    Wire.endTransmission();
+    delay(50);
 }
+
 // read humidity and temperature data from HIH7120
 void measureHIH7120(){
-  if (Wire.available()==4){
-    for (int i = 0; i <4; i++){
-      buf[i]=Wire.read();
+    if (Wire.available()==4){
+        for (int i = 0; i <4; i++){
+            buf[i]=Wire.read();
+        }
+        if(!bitRead(buf[0], 7)&&bitRead(buf[0], 6)){
+            Serial.println("Data is stale");
+        }
+    } else {
+        Serial.print("Error in data.\n");
     }
-    if(!bitRead(buf[0], 7)&&bitRead(buf[0], 6)){
-      Serial.println("Data is stale");
-    }
-  } else {
-    Serial.print("Error in data.\n");
-  }
 }
+
 // initialize Mpl sensor based on sample program from data sheet
 void setupMPL(){
-  Wire.beginTransmission(MPLADDRESS);
-  Wire.write(0x26);
-  Wire.write(0x39); //Turn on and enable oversampling
-  Wire.endTransmission();
-  delay(30);
+    Wire.beginTransmission(MPLADDRESS);
+    Wire.write(0x26);
+    Wire.write(0x39); //Turn on and enable oversampling
+    Wire.endTransmission();
+    delay(30);
 }
 
 pressure_StructDef readPressureMPL() {
-  Wire.beginTransmission(MPLADDRESS);
-  Wire.write(0);
-  Wire.requestFrom(MPLADDRESS, 4);
-  delay(30);
-  if (Wire.available()) {
-    Wire.read(); //discard the first byte, it doesn't matter
-    //readings start at 0x01
-    OUT_P_MSB = Wire.read();
-    OUT_P_CSB = Wire.read();
-    OUT_P_LSB = Wire.read();
-    /*
-    * 20 bit value, with 18 bits + 2 bits fractional value
-    * MSB [7:0], CSB [7:0], and LSB [7:6] make up 18 bits
-    * LSB [5:4] make up the fractional component
-    */
-    pressure.whole = 0;
-    pressure.whole += (OUT_P_MSB << 10);
-    pressure.whole += (OUT_P_CSB << 2);
-    pressure.whole += (OUT_P_LSB >> 6);
+    Wire.beginTransmission(MPLADDRESS);
+    Wire.write(0);
+    Wire.requestFrom(MPLADDRESS, 4);
+    delay(30);
+    if (Wire.available()) {
+        Wire.read(); //discard the first byte, it doesn't matter
+        //readings start at 0x01
+        OUT_P_MSB = Wire.read();
+        OUT_P_CSB = Wire.read();
+        OUT_P_LSB = Wire.read();
+        /*
+        * 20 bit value, with 18 bits + 2 bits fractional value
+        * MSB [7:0], CSB [7:0], and LSB [7:6] make up 18 bits
+        * LSB [5:4] make up the fractional component
+        */
+        pressure.whole = 0;
+        pressure.whole += (OUT_P_MSB << 10);
+        pressure.whole += (OUT_P_CSB << 2);
+        pressure.whole += (OUT_P_LSB >> 6);
 
-    pressure.fractional = 0;
-    //only bytes 4 and 5 are relevant, so bitshift 4 right
-    //then clear everything other than 4 and 5
-    pressure.fractional = ((OUT_P_LSB >> 4) & 0b00000011) / 4;
-  }
-  Wire.endTransmission();
-  return pressure;
+        pressure.fractional = 0;
+        //only bytes 4 and 5 are relevant, so bitshift 4 right
+        //then clear everything other than 4 and 5
+        pressure.fractional = ((OUT_P_LSB >> 4) & 0b00000011) / 4;
+    }
+    Wire.endTransmission();
+    return pressure;
 }
 
 float publishTemp() {
@@ -121,33 +124,31 @@ float publishPress() {
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  Wire.begin();
+    Wire.begin();
 
-  initLeds();
+    initLeds();
 
-  Serial.begin(9600);
-  delay(5000);
-  Serial.println("Setup start");
-  
-  setupMPL();
+    Serial.begin(9600);
+    delay(5000);
+    Serial.println("Setup start");
+    
+    setupMPL();
 
-  // Create a node
-  systemClock = &getSystemClock();
-  canDriver = &getCanDriver();
-  node = new Node<NodeMemoryPoolSize>(*canDriver, *systemClock);
-  initNode(node, nodeID, nodeName, swVersion, hwVersion);
+    // Create a node
+    systemClock = &getSystemClock();
+    canDriver = &getCanDriver();
+    node = new Node<NodeMemoryPoolSize>(*canDriver, *systemClock);
+    initNode(node, nodeID, nodeName, swVersion, hwVersion);
 
-  // init subscriber
-  initPublisher(node);
+    // init subscriber
+    initPublisher(node);
 
-  // start up node
-  node->setModeOperational();
-  Serial.println("Setup complete");
+    // start up node
+    node->setModeOperational();
+    Serial.println("Setup complete");
 }
 
 void loop() {
-
     hum_measurement_req();
     Wire.requestFrom(HIH7120ADDRESS, 4);
     measureHIH7120();
@@ -174,10 +175,7 @@ void loop() {
     // toggle heartbeat
     toggleHeartBeat();
 
-  if(timer.check() == 1) {
-    cyclePublisher();
-    timer.reset();
-  }
-
-
+    if(timer.check() == 1) {
+      cyclePublisher();
+    }
 }
