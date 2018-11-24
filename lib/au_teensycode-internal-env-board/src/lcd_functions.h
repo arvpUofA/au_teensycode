fc3bb6491e9adc478281e7df84bd9d13484ff15e
Kyle <kylercollins2@gmail.com>
2018-11-24 11:37:39 -0700

    Updated env board with lcd code
    Added on the LCD display code to what Alex had

--- lib/au_teensycode-internal-env-board/src/lcd_functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef LCD_FUNCTIONS_H
-#define LCD_FUNCTIONS_H
-
-
-
-extern void setup_lcd(void);
-extern void display_to_lcd(float temp,float hum,float pressure, float volt);
-extern void display_voltages_to_lcd(float battery1, float battery2, float battery3, float battery4);
