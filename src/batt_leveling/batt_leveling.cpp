4785a0fbe288cf2950c740688d5c386808436f8c
0x766F6964 <palamar@ualberta.ca>
2020-03-14 14:11:28 -0600

    add store_voltage_info() and voltcmp() functions
    also initialize subscibers for all batteries
    
    remaining TODO is the voltage src swapping function

--- src/batt_leveling/batt_leveling.cpp
+++ src/batt_leveling/batt_leveling.cpp
@@ -6,42 +6,68 @@
 
 #include "batt_leveling.h"
 
-#define NUM_OF_BATTERIES 4
-
-void storeVoltageInfo(uint8_t i, float f) {};
-
 static const char *nodename = "org.arvp.batteryleveling";
 
-static Subscriber<equipment::power::BatteryInfo> *battSubscriber;
-static Publisher<equipment::power::BatteryInfo> *battPublisher;
+static Subscriber<equipment::power::BatteryInfo> *batt_subscriber;
+static Publisher<equipment::power::BatteryInfo> *batt_publisher;
+
+static float voltages[NUM_OF_BATTERIES];
+
+/* swaps batt src and updates src field */
+static void
+batt_swap(uint8_t id_1, uint8_t id_2)
+{
+	/* TODO */
+}
+
+/* returns 1 if v_1 src and v_2 src should be swapped */
+static int
+voltcmp(float v_1, float v_2)
+{
+	return abs(v_1 - v_2) > V_DELTA ? 1 : 0;
+}
+
+/* store voltage level in voltage array */
+static void
+store_voltage_info(uint8_t i, float f)
+{
+	voltages[i] = f;
+}
 
 /* Callback function for uavcan battery info messages. Stores values into storage array */
 static void
-batteryInfoCallback(const uavcan::equipment::power::BatteryInfo& batteryData)
+batt_info_callback(const uavcan::equipment::power::BatteryInfo& batt_data)
 {
-	storeVoltageInfo(batteryData.battery_id, batteryData.voltage);
+	store_voltage_info(batt_data.battery_id, batt_data.voltage);
 }
 
 static void 
-initSubscriber(Node<NodeMemoryPoolSize> *node)
+init_subscriber(Node<NodeMemoryPoolSize> *node)
 {
-	battSubscriber = new Subscriber<equipment::power::BatteryInfo>(*node);
+	int i;
+	Subscriber<equipment::power::BatteryInfo> *batt_sub = batt_subscriber;
+	for (i = 0; i < NUM_OF_BATTERIES; i++) {
+		batt_sub = new Subscriber<equipment::power::BatteryInfo>(*node);
 
-	if (battSubscriber->start(batteryInfoCallback) < 0)
-		Serial.println("Unable to start battery subscriber");
+		if (batt_sub->start(batt_info_callback) < 0)
+			Serial.printf("Battery [%d]: failed to start subscriber\n", i);
+
+		batt_sub++;
+	}
+
 }
 
 static void
-initPublisher(Node<NodeMemoryPoolSize> *node)
+init_publisher(Node<NodeMemoryPoolSize> *node)
 {
 	int i;
-	Publisher<equipment::power::BatteryInfo> *batt_info = battPublisher;
+	Publisher<equipment::power::BatteryInfo> *batt_info = batt_publisher;
 
 	for (i = 0; i < NUM_OF_BATTERIES; i++) {
 		batt_info = new Publisher<equipment::power::BatteryInfo>(*node);
 
         	if (batt_info->init() < 0)
-			Serial.println("Unable to initialize battery message publisher!");
+			Serial.printf("Battery [%d]: failed to start publisher\n", i);
 
 		batt_info->setTxTimeout(MonotonicDuration::fromUSec(800));
 
@@ -63,8 +89,8 @@ setup(void)
 	node = new Node<NodeMemoryPoolSize>(*canDriver, *systemClock);
 	initNode(node, UAVCAN_NODE_ID_BATTERY_LEVELING_BOARD, nodename, SW_VERSION, HW_VERSION);
 
-	initSubscriber(node);
-	initPublisher(node);
+	init_subscriber(node);
+	init_publisher(node);
 
 	/* set up filters (must be after publisher & subscriber setup!) */
 	configureCanAcceptanceFilters(*node);
@@ -79,4 +105,11 @@ setup(void)
 void
 loop(void)
 {
+	int i;
+	for (i = 1; i < NUM_OF_BATTERIES; i++) {
+		if (voltcmp(voltages[0], voltages[i]))
+			batt_swap(0, i);
+	}
+
+	delay(500);
 }
--- src/batt_leveling/batt_leveling.h
+++ src/batt_leveling/batt_leveling.h
@@ -1,3 +1,9 @@
 /* batt_leveling firmware info */
+#define ENABLE_BATT_DEBUG 0
 #define SW_VERSION 1
 #define HW_VERSION 1
+
+#define NUM_OF_BATTERIES 4
+
+/* Difference in voltage needed to request battery swap */
+#define V_DELTA 0.01
