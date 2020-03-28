8bbbfd24497f2b116cd78757aeea6bd256de6000
0x766F6964 <palamar@ualberta.ca>
2020-03-28 14:18:40 -0600

    MOTOR_BATT_CONT is pin 6 not pin 11

--- src/batt_leveling/batt_leveling.h
+++ src/batt_leveling/batt_leveling.h
@@ -12,6 +12,6 @@
 #define V_DELTA 0.01
 
 /* Pin definitions */
-/* 10 should != 11 this will cause a short */
+/* 10 should != 6 this will cause a short */
 #define EE_BATT_CONT 10
-#define MOTOR_BATT_CONT 11
+#define MOTOR_BATT_CONT 6
